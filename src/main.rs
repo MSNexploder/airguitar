95574a665b3e0e9c710fdbc6c42f1deb80c35c1e
Stefan Stüben <MSNexploder@gmail.com>
2021-10-20 14:26:10 +0200

    Let's introduce ourself

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+use tokio::{net::TcpListener, signal};
+
+mod server;
+
+mod shutdown;
+use shutdown::Shutdown;
+
+mod mdns;
+use mdns::Mdns;
+
+#[tokio::main]
+async fn main() -> crate::Result<()> {
+    let port = 0; // don't care for now
+    let listener = TcpListener::bind(&format!("0.0.0.0:{}", port)).await?;
+
+    server::run(listener, signal::ctrl_c()).await
 }
+
+pub type Result<T> = std::result::Result<T, Error>;
